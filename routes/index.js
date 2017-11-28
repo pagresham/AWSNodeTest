5f0bbf222c42d2cc86c8292b0bca6861089c8ff0
pierce gresham <piercegresham@gmail.com>
2017-11-28 18:24:43 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express',
+			message: "Welcome to Node on AWS" });
+});
+
+module.exports = router;
