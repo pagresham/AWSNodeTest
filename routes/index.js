44862cbe232472c43982757cca6d95b89b002490
pierce gresham <piercegresham@gmail.com>
2017-11-28 23:20:29 -0500

    added messeges to slash and api routes

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express',
-			message: "Welcome to Node on AWS" });
+  //res.render('index', { title: 'Express',
+  //		message: "Welcome to Node on AWS" });
+  res.send('Please use the /api endpoint')
 });
+
+router.get('/api', function(req, res) {
+	res.send("Hope you day is a real trip!");
+}
 
 module.exports = router;
